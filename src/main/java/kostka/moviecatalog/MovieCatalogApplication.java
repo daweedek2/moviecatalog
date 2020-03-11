f3985c46682dbecbeabe991fe9d2f05d4da9c82b
kostka <dkostka@eurofunk.com>
2020-03-11 08:47:32 +0100

    first draft of the ELK config

--- src/main/java/kostka/moviecatalog/MovieCatalogApplication.java
+++ src/main/java/kostka/moviecatalog/MovieCatalogApplication.java
@@ -1,13 +1,18 @@
 package kostka.moviecatalog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieCatalogApplication {
-
     public static void main(final String[] args) {
+        Logger LOGGER = LoggerFactory.getLogger("jsonLogger");
         SpringApplication.run(MovieCatalogApplication.class, args);
+        LOGGER.info("moviecatalog is running :)");
+        LOGGER.debug("Debug message");
+        LOGGER.trace("Trace message");
     }
 
 }
