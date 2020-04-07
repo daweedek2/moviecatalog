7cb6b315956e68cfe66d130a14dd1c26690aadba
kostka <dkostka@eurofunk.com>
2020-04-07 14:23:09 +0200

    #28 Use JPA specifications to filter the movies

--- src/main/java/kostka/moviecatalog/repository/MovieRepository.java
+++ src/main/java/kostka/moviecatalog/repository/MovieRepository.java
@@ -2,12 +2,13 @@ package kostka.moviecatalog.repository;
 
 import kostka.moviecatalog.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
     List<Movie> findMovieByName(String name);
     List<Movie> findByIdInOrderByIdDesc(List<Long> ids);
     List<Movie> findByIdInOrderByRatingDesc(List<Long> ids);
