cbf13936407544bc6f290984a8e0ac5f0466f23a
kostka <dkostka@eurofunk.com>
2020-04-29 10:13:54 +0200

    Implement field errors for the form fields.

--- src/test/java/kostka/moviecatalog/service/Generator.java
+++ src/test/java/kostka/moviecatalog/service/Generator.java
@@ -25,7 +25,7 @@ public class Generator {
 
     public RatingDto createValidRatingDto(final Long id, final int rating) {
         RatingDto dto = new RatingDto();
-        dto.setMovieId(id);
+        dto.setId(id);
         dto.setRatingValue(rating);
         return dto;
     }
