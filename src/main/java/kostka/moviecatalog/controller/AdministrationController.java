4379fa32b76e8963d9679293485aa205b29a8f56
David Kostka <dkostka@eurofunk.com>
2021-05-28 20:19:36 +0200

    commit 2 to release

--- src/main/java/kostka/moviecatalog/controller/AdministrationController.java
+++ src/main/java/kostka/moviecatalog/controller/AdministrationController.java
@@ -86,6 +86,7 @@ public class AdministrationController {
         rabbitMqSender.sendRefreshAdminRequestToQueue();
         redirectAttributes.addFlashAttribute(SUCCESS, "Movie is successfully created.");
         return REDIRECT_ADMIN_VIEW;
+        // another release change
     }
 
     /**
